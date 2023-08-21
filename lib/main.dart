c6209504b3cb76466e9bbbc65e40d3f57f0dc166
Neta4302 <54069403+Neta4302@users.noreply.github.com>
2023-08-21 15:16:49 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:project1/constant/my_constant.dart';
+//import 'package:project1/screens/home_screen.dart';
+import 'package:project1/screens/bottomnavbar_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Project',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: MyConstant.success,
+        primaryColor: MyConstant.info,
+      ),
+      home: const BottomNavBar(),
+    );
+  }
+}
